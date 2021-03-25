3975acd235d9dfb761da7b3d04b1698897a3190a
torjc01 <julio_cesar.torres@sct.gouv.qc.ca>
2021-03-25 13:17:37 -0400

    Corrige version et emplacement de la LICENSE

--- MFA-controller/src/Inscription/containers/VerificationPreuveContainer.js
+++ MFA-controller/src/Inscription/containers/VerificationPreuveContainer.js
@@ -1,8 +1,8 @@
 /*
 * Copyright (c) 2020 Gouvernement du Québec
 * Auteur: Julio Cesar Torres (torj01)
-* SPDX-License-Identifier: LiLiQ-P-v.1.1
-* License-Filename: LICENSES/LILIQ-P11ENunicode.txt
+* SPDX-License-Identifier: LiLiQ-R-v.1.1
+* License-Filename: /LICENSE
 */
 import React, { useState, useEffect }  from 'react';
 import { Container, Spinner }          from 'reactstrap'
